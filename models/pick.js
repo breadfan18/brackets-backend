0da35e24c72e1d228931535a0dcac387a41bd627
Swaroop <breadfan18@gmail.com>
2021-05-31 11:30:47 -0400

    minor refactoring

--- models/pick.js
+++ models/pick.js
@@ -16,30 +16,3 @@ const picksSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Pick', picksSchema);
 
-const group = {
-    'A': {
-        1: 'England',
-        2: 'Spain',
-        3: 'France',
-        4: 'Portugal'
-    },
-    'B': {
-        1: 'England',
-        2: 'Spain',
-        3: 'France',
-        4: 'Portugal'
-    },
-    'C': {
-        1: 'England',
-        2: 'Spain',
-        3: 'France',
-        4: 'Portugal'
-    },
-    'D': {
-        1: 'England',
-        2: 'Spain',
-        3: 'France',
-        4: 'Portugal'
-    },
-
-}
