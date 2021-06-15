2c2871c0e46b73f1721c494c252f156f15600c89
Swaroop <breadfan18@gmail.com>
2021-06-15 15:05:04 -0400

    minor

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const picksCtrl = require('../controllers/picks');
+const cors = require('cors');
 
 router.get('/', picksCtrl.indexUser);
 router.get('/all', picksCtrl.indexAll);
 router.post('/', picksCtrl.create);
-router.put('/:id', picksCtrl.update);
+router.put('/:id', cors(), picksCtrl.update);
 router.delete('/:id', picksCtrl.delete);
 
 module.exports = router;
